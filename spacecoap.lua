e4d40a18ceb1a34ec143b7af0a577d3567270f51
Michael Karpov <karpovmichael@outlook.com>
2025-05-06 14:50:36 +0200

    Added License to dissector

--- spacecoap.lua
+++ spacecoap.lua
@@ -1,4 +1,17 @@
--- Space CoAP Lua Dissector (CoAP with 24-bit Message ID and payload_length option)
+-- Highly experimental Space CoAP Lua Dissector (CoAP with 24-bit Message ID and payload_length option)
+-- Copyright (C) 2025  Michael Karpov
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 -- Define the new protocol
 local spacecoap_proto = Proto("spacecoap", "Space CoAP Protocol")
